7261a403c00b209bbe50691c308583cf2ca6a62d
Adam Swann <adam@velocity2.com>
2022-04-29 08:09:02 -0500

    Stab at power control (untested on an actual pi).

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/on", () => HdmiOn());
+app.MapGet("/off", () => HdmiOff());
 
 app.Run();
+
+string HdmiOn() {
+    SetDisplayPowerState(true);
+    return "OK, on";
+}
+
+string HdmiOff() {
+    SetDisplayPowerState(false);
+    return "OK, off";
+}
+
+void SetDisplayPowerState(bool powerOn)
+{
+
+    var state = powerOn ? "1" : "0";
+    
+    ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/usr/bin/vcgencmd", Arguments = $"display_power {state}" }; 
+    Process proc = new Process() { StartInfo = startInfo, };
+    proc.Start();
+}
